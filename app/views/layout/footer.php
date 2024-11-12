60c1e387f006ec2d01db0f57abdcfd25829c3f64
Oliver Ros <seair75@gmail.com>
2024-11-12 16:39:54 +0100

    Footer responsive solved (shure)

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,13 +1,13 @@
-<footer class="w-full px-20 pt-5 grid grid-rows-3 lg:grid-rows-3 grid-cols-1 lg:grid-cols-2 auto-rows-max auto-cols-max lg:gap-x-10 gap-y-3">
-    <div class="lg:row-span-2 w-full md:px-20 grid md:grid-cols-2 md:auto-cols-max justify-center gap-6">
-        <ul class="md:w-fit flex flex-col gap-2">
+<footer class="w-full px-20 pt-5 grid grid-rows-4 md:grid-rows-3 grid-cols-1 lg:grid-cols-2 auto-rows-max auto-cols-max lg:gap-x-10 gap-y-3">
+    <div class="row-span-2 md:row-span-1 w-full w-fit px-36 grid grid-cols-1 md:grid-cols-2 md:auto-cols-max justify-items-center gap-4 md:gap-6">
+        <ul class="w-fit grid gap-y-4 justify-items-center md:justify-items-start md:gap-y-2">
             <li>Home</li>
             <li>Menu</li>
             <li>Offers</li>
             <li>About us</li>
             <li>Contact us</li>
         </ul>
-        <ul class="md:w-fit flex flex-col gap-2">
+        <ul class="w-fit grid gap-y-4 justify-items-center md:justify-items-start md:auto-rows-max md:gap-y-2">
             <li>Terms and conditions</li>
             <li>Privacy policy</li>
         </ul>
@@ -19,5 +19,5 @@
         </a>
         <a class="btn btn-secondary text-md lg:text-lg font-bold" role="button" href="">Contact us</a>
     </div>
-    <small class="row-start-3 justify-self-center self-center col-span-2 h-fit">&copy; SymfonyRestaurant 2024 All rights reserved</small>
+    <small class="justify-self-center self-center col-span-2 h-fit">&copy; SymfonyRestaurant 2024 All rights reserved</small>
 </footer>
--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,3 +1,9 @@
 <section class="h-90 grid lg:grid-rows-3 lg:grid-cols-2">
-    <div class="hidden lg:flex "></div>
+    <div class="grid lg:grid-rows-4">
+        <h1>Reach the infinite with our dishes variety</h1>
+        <p>We have a lot of dishes, from principals to desserts</p>
+        <button class="btn btn-secondary"></button>
+        <button class="btn btn-tertiary"></button>
+    </div>
+    <div class="hidden lg:flex"></div>
 </section>
