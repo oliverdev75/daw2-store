5d90b4208c300beccfc049e7d0f194d5cf019b36
Oliver Ros <seair75@gmail.com>
2024-11-14 15:19:21 +0100

    Landing second section missing responsive

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -1,6 +1,6 @@
 <section class="py-20 lg:p-0 grid lg:grid-rows-1 lg:grid-cols-2 lg:gap-5 items-start">
     <div class="w-full lg:w-fit h-full grid lg:grid-cols-1 justify-center lg:justify-start content-center auto-rows-min gap-y-5 text-center md:text-left">
-        <h1 class="text-[2rem] lg:text-6xl text-white lg:text-[#303952]">Reach the infinite with our dishes variety</h1>
+        <h1 class="text-[2rem] lg:text-6xl text-white text-[#303952]">Reach the infinite with our dishes variety</h1>
         <div class="w-full lg:w-fit px-3 lg:p-0 grid lg:grid-rows-2 auto-rows-max gap-y-3 lg:gap-y-2 text-center lg:text-start">
             <p class="text-lg lg:text-2xl">We have a lot of dishes, from principals to desserts</p>
             <div class="w-full lg:w-fit grid grid-flow-col auto-rows-max auto-cols-max justify-center lg:justify-start gap-x-5">
@@ -13,9 +13,11 @@
         <img class="object-contain" src="<?= $this->image('cat_space') ?>" alt="Astronaut cat in space">
     </div>
 </section>
-<section class="lg:py-20 grid lg:grid-rows-3 lg:grid-cols-2 auto-rows-max grid-flow-col justify-items-center">
-    <h3 class="lg:row-start-1 lg:col-span-2 lg:text-5xl h-fit text-center">Enjoy the best sea flavors!</h3>
-    <img class="w-80 lg:row-start-2 lg:col-span-2 justify-self-center" src="<?= $this->image('ceviche') ?>" alt="Ceviche">
-    <img class="w-80 lg:row-start-3" src="<?= $this->image('prawns') ?>" alt="Lemmon prawns">
-    <img class="w-80 lg:row-start-3" src="<?= $this->image('salmon') ?>" alt="Salmon">
+<section class="lg:py-20 grid lg:grid-rows-4 auto-rows-max justify-center">
+    <h3 class="lg:text-5xl text-center text-[#303952]">Enjoy the best sea flavors!</h3>
+    <div class="lg:row-span-3 grid lg:grid-rows-2 lg:grid-cols-2 lg:gap-x-10 lg:gap-y-5">
+        <img class="w-80 lg:col-span-2 justify-self-center" src="<?= $this->image('ceviche') ?>" alt="Ceviche">
+        <img class="w-80 lg:row-start-2 lg:col-start-1" src="<?= $this->image('salmon') ?>" alt="Salmon">
+        <img class="w-80 lg:row-start-2 lg:col-start-2" src="<?= $this->image('prawns') ?>" alt="Lemmon prawns">
+    </div>
 </section>
