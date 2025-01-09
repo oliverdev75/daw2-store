68763285c4ab294e0833e6d4ec0bb1d0b377d759
Óliver Ros <105983648+oliverdev75@users.noreply.github.com>
2025-01-09 16:49:17 +0100

    Final commit

--- app/src/Controllers/UserController.php
+++ app/src/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
     static function logout()
     {
         $user = self::current();
-        if ($user) {
+        if (!$user) {
             return Send::redirect();
         }
 
